8c8b4eda20026aa04436436f424152e1496a5a86
Fizza Nawaz <163080358+FizzaNawaz-167@users.noreply.github.com>
2024-03-24 14:58:59 +0500

    Update Todo.jsx

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -42,7 +42,7 @@ export default function Todo() {
                 style={todo.isUpdated ? { backgroundColor: "green" } : {}}
                 onClick={() => dispatch(updateSingleTodo(todo.id))}
               >
-                Updateded
+                Updated
               </button>
             </li>
           ))}
